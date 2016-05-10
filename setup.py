20b913c0c248804cf00746d6cb8a429d07985bcf
Panu Tulimäki <panu.tulimaki@anders.fi>
2016-05-10 08:58:49 -0700

    Fix version 2.0 installation
    This should enable migrations when installing with pip install
    
    Refs SHOOP-2478

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         name="shoop-checkoutfi",
         version="0.2",
         description="Shoop Checkout.fi Integration",
-        packages=["shoop_checkoutfi"],
+        packages=setuptools.find_packages(),
         include_package_data=True,
         install_requires=["shoop>=3.0.0.post0.dev233,<5.0"],
         entry_points={"shoop.addon": "shoop_checkoutfi=shoop_checkoutfi"}
