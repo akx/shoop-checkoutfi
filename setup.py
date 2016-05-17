125de604a03a9bef3e520062a2ce7f83aafd9438
Tuomas Suutari <tuomas.suutari@andersinno.fi>
2016-05-17 15:38:03 +0300

    Make sure additional resources get installed
    Make setup.py more future-proof by making it compile translations (*.po
    to *.mo) and include them with a manifest file.
    
    Refs SHOOP-2565

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
 
+try:
+    import shoop_setup_utils
+except ImportError:
+    shoop_setup_utils = None
+
+
 if __name__ == '__main__':
     setuptools.setup(
         name="shoop-checkoutfi",
@@ -7,6 +13,9 @@ if __name__ == '__main__':
         description="Shoop Checkout.fi Integration",
         packages=setuptools.find_packages(),
         include_package_data=True,
-        install_requires=["shoop>=3.0.0.post0.dev233,<5.0"],
-        entry_points={"shoop.addon": "shoop_checkoutfi=shoop_checkoutfi"}
+        entry_points={"shoop.addon": "shoop_checkoutfi=shoop_checkoutfi"},
+        cmdclass=(shoop_setup_utils.COMMANDS if shoop_setup_utils else {}),
+        install_requires=[
+            'shoop>=3.0.0.post0.dev233,<5.0',
+        ],
     )
